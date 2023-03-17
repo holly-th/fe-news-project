5e1cebd4be75d2d2969b771237aa201eaeacfc54
holly-th <holly.thomas135@gmail.com>
2023-03-17 14:59:43 +0000

    added the fetching for userpage

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ArticleCard from "./ArticleCard";
 import Header from "./Header";
 import Nav from "./Nav";
 import Topics from "./Topics";
-
+import Users from "./Users";
 function App() {
   return (
     <div className="App">
@@ -15,6 +15,7 @@ function App() {
         <Route path="/" element={<Articles />} />
         <Route path="/articles" element={<Articles />} />
         <Route path="/topics" element={<Topics />} />
+        <Route path="/users" element={<Users />} />
         <Route path="/articles/:article_id" element={<ArticleCard />} />
       </Routes>
     </div>
---
+++ src/Users.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { getUsers } from "./utils/api";
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    getUsers().then((usersData) => {
+      setUsers(usersData);
+    });
+  }, []);
+  console.log(users);
+  return users.map((user) => {
+    return (
+      <li className="user" key={user.username}>
+        <p className="userpageUsername">{user.username}</p>
+        <p className="userpageName">{user.name}</p>
+        <img
+          className="userpageImage"
+          src={user.avatar_url}
+          alt={user.username}
+        ></img>
+      </li>
+    );
+  });
+}
+export default Users;
